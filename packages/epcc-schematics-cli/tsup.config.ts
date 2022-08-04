7c4daa6ac435d5cc236da539299c71982b02fb07
Robert Field <robertfield@quadrimular.com>
2022-08-04 11:24:28 +0100

    feat: simple schematic cli

---
+++ packages/epcc-schematics-cli/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tsup"
+import { copy } from "esbuild-plugin-copy"
+
+export default defineConfig(({ env }) => {
+  return {
+    outExtension({ format }) {
+      return {
+        js: `.js`,
+      }
+    },
+    entry: ["./schematics.ts"],
+    format: ["cjs"],
+    dts: true,
+    clean: true,
+    sourcemap: false,
+    esbuildPlugins: [
+      copy({
+        // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+        // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+        resolveFrom: "cwd",
+        assets: {
+          from: ["./blank/**/*", "./schematic/**/*"],
+          to: ["./dist"],
+        },
+      }),
+    ],
+  }
+})
