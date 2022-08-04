770d3a009c589aaa2d52391192cbaa8ea24d4b9f
Robert Field <robertfield@quadrimular.com>
2022-08-04 12:06:50 +0100

    build: output to match example

--- packages/epcc-schematics-cli/tsup.config.ts
+++ packages/epcc-schematics-cli/tsup.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ env }) => {
       }
     },
     entry: ["./src/schematics.ts"],
+    outDir: "./dist/bin",
     format: ["cjs"],
     dts: true,
     clean: true,
@@ -18,10 +19,30 @@ export default defineConfig(({ env }) => {
         // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
         // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
         resolveFrom: "cwd",
-        assets: {
-          from: ["./blank/**/*", "./schematic/**/*"],
-          to: ["./dist"],
-        },
+        assets: [
+          {
+            from: ["./collection.json"],
+            to: ["./dist/collection.json"],
+          },
+          {
+            from: ["./package.json"],
+            to: ["./dist/package.json"],
+          },
+          {
+            from: ["./README.md"],
+            to: ["./dist/README.md"],
+          },
+          {
+            from: ["./src/blank/**/*"],
+            to: ["./dist/blank"],
+            keepStructure: true,
+          },
+          {
+            from: ["./src/schematic/**/*"],
+            to: ["./dist/schematic"],
+            keepStructure: true,
+          },
+        ],
       }),
     ],
   }
