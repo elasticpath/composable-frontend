8044488f555ccc4e5159d2d93f5be0fd119f017f
Robert Field <robertfield@quadrimular.com>
2022-09-07 14:24:17 +0100

    refactor: remove d2c example from repository

--- examples/d2c-algolia/src/lib/search-client.ts
+++
@@ -1,7 +0,0 @@
-import algoliasearch from "algoliasearch/lite";
-import { algoliaEnvData } from "./resolve-algolia-env";
-
-export const searchClient = algoliasearch(
-  algoliaEnvData.appId,
-  algoliaEnvData.apiKey
-);
--- examples/d2c-algolia/src/pages/category/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const Categories: NextPage = () => {
-  return <p>Categories page coming soon.</p>;
-};
-
-export default Categories;
