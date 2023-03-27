3ae27342a0a3d82f468fb958c0624ea378b0bc70
Robert Field <robertfield@quadrimular.com>
2023-03-27 12:26:07 +0100

    fix: added correct extension to deployer url

--- packages/mason-common/src/integration-hub/constants.ts
+++ packages/mason-common/src/integration-hub/constants.ts
@@ -4,4 +4,4 @@ export const ALGOLIA_INTEGRATION_ID =
   "SW50ZWdyYXRpb246NjQ2Y2VhYjgtODgxMS00MDM3LWE5NjItZDYwMTczNTJkOTJi"
 
 export const MASON_INTEGRATION_HUB_DEPLOYER_URL =
-  "https://mason-integration-hub-deployer.netlify.app"
+  "https://mason-integration-hub-deployer.netlify.app/.netlify/functions"
