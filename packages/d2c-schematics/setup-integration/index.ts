25698f99c55c7c33ae3ee81882dd572a762a4a44
Robert Field <robertfield@quadrimular.com>
2023-03-28 12:57:14 +0100

    feat: use correct logging

--- packages/d2c-schematics/setup-integration/index.ts
+++ packages/d2c-schematics/setup-integration/index.ts
@@ -1,70 +1,18 @@
-import {
-  apply,
-  chain,
-  filter,
-  MergeStrategy,
-  mergeWith,
-  move,
-  Rule,
-  SchematicContext,
-  Tree,
-  url,
-} from "@angular-devkit/schematics"
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics"
 import { Schema as SetupIntegrationOptions } from "./schema"
-import { setupAlgoliaIntegration } from "../utility/integration-hub/setup-algolia-integration"
-import { algoliaIntegrationSettingsSchema } from "@elasticpath/mason-common"
-import { addEnvVariables } from "../utility/add-env-variable"
-import * as path from "path"
+import { createFallbackLogger } from "../utility/fallback-logger"
+import { performAlgoliaIntegrationSetup } from "../utility/integration-hub/perform-algolia-integration-setup"
 
 export default function (options: SetupIntegrationOptions): Rule {
-  return async (host: Tree, _context: SchematicContext): Promise<Rule> => {
-    console.log("options: ", options)
-    console.log("host passed to setup integration schematic: ", host)
+  return async (host: Tree, context: SchematicContext): Promise<Rule> => {
+    const logger = context.logger ?? createFallbackLogger()
     switch (options.integrationName) {
       case "algolia":
-        return performAlgoliaIntegrationSetup(options, host)
+        return performAlgoliaIntegrationSetup(options, host, logger)
       default:
         return Promise.reject(
           `Failed to setup integration, unsupported integration name: ${options.integrationName}`
         )
     }
   }
-}
-
-export const ALGOLIA_INDEX_NAME = "NEXT_PUBLIC_ALGOLIA_INDEX_NAME"
-
-async function performAlgoliaIntegrationSetup(
-  options: SetupIntegrationOptions,
-  _host: Tree
-): Promise<Rule> {
-  const parsedOptionsResult =
-    algoliaIntegrationSettingsSchema.safeParse(options)
-
-  if (!parsedOptionsResult.success) {
-    return Promise.reject(parsedOptionsResult.error)
-  }
-
-  const parsedOptions = parsedOptionsResult.data
-
-  const result = await setupAlgoliaIntegration(parsedOptions)
-
-  // TODO perform logic to report the status of the configuration to the user and including success and failure states
-
-  // TODO work out the actual index name when we configure integration hub Algolia
-  const algoliaIndexName = ""
-
-  const directoryPath = path.join(process.cwd(), options.directory)
-
-  return chain([
-    mergeWith(
-      apply(url(directoryPath), [
-        filter((path) => path === "/.env.local"),
-        addEnvVariables({
-          [ALGOLIA_INDEX_NAME]: algoliaIndexName,
-        }),
-        move(options.directory),
-      ]),
-      MergeStrategy.Overwrite
-    ),
-  ])
 }
---
+++ packages/d2c-schematics/utility/integration-hub/perform-algolia-integration-setup.ts
@@ -0,0 +1,61 @@
+import { Schema as SetupIntegrationOptions } from "../../../../dist-schema/packages/d2c-schematics/setup-integration/schema"
+import {
+  apply,
+  chain,
+  filter,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  Tree,
+  url,
+} from "@angular-devkit/schematics"
+import { logging } from "@angular-devkit/core"
+import { algoliaIntegrationSettingsSchema } from "@elasticpath/mason-common"
+import { setupAlgoliaIntegration } from "./setup-algolia-integration"
+import { formatAlgoliaIntegrationResponse } from "./format-algolia-integration-response"
+import path from "path"
+import { addEnvVariables } from "../add-env-variable"
+
+export const ALGOLIA_INDEX_NAME = "NEXT_PUBLIC_ALGOLIA_INDEX_NAME"
+
+export async function performAlgoliaIntegrationSetup(
+  options: SetupIntegrationOptions,
+  _host: Tree,
+  logger: logging.LoggerApi
+): Promise<Rule> {
+  const parsedOptionsResult =
+    algoliaIntegrationSettingsSchema.safeParse(options)
+
+  if (!parsedOptionsResult.success) {
+    return Promise.reject(parsedOptionsResult.error)
+  }
+
+  const parsedOptions = parsedOptionsResult.data
+
+  const result = await setupAlgoliaIntegration(parsedOptions)
+
+  if (!result.success) {
+    logger.error(formatAlgoliaIntegrationResponse(result))
+  } else {
+    logger.info(formatAlgoliaIntegrationResponse(result))
+  }
+
+  // TODO work out the actual index name when we configure integration hub Algolia
+  const algoliaIndexName = ""
+
+  const directoryPath = path.join(process.cwd(), options.directory)
+
+  return chain([
+    mergeWith(
+      apply(url(directoryPath), [
+        filter((path) => path === "/.env.local"),
+        addEnvVariables({
+          [ALGOLIA_INDEX_NAME]: algoliaIndexName,
+        }),
+        move(options.directory),
+      ]),
+      MergeStrategy.Overwrite
+    ),
+  ])
+}
