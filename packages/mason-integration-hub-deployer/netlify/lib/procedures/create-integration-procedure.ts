b124fb2829962e11d2bdebba9626d13852488879
Robert Field <robertfield@quadrimular.com>
2023-03-28 17:27:56 +0100

    fix: try catch for catching unexpected errors in handler

--- packages/mason-integration-hub-deployer/netlify/lib/procedures/create-integration-procedure.ts
+++ packages/mason-integration-hub-deployer/netlify/lib/procedures/create-integration-procedure.ts
@@ -58,120 +58,124 @@ export async function algoliaCreateIntegrationHandler(
     appId,
     adminApiKey,
   } = config
-  const customerUrqlClient = createUrqlClient(creds, host)
 
-  /**
-   * Validate the customer token by getting user info
-   */
-  const userInfo = await getUserInfo(customerUrqlClient)
-  logger.info(`userInfo: ${JSON.stringify(userInfo)}`)
+  try {
+    const customerUrqlClient = createUrqlClient(creds, host)
 
-  logger.info(
-    `request: ${JSON.stringify(
-      !userInfo.success ? userInfo.error.message : ""
-    )}`
-  )
+    /**
+     * Validate the customer token by getting user info
+     */
+    const userInfo = await getUserInfo(customerUrqlClient)
+    logger.info(`userInfo: ${JSON.stringify(userInfo)}`)
 
-  if (didRequestFail(userInfo)) {
-    return resolveErrorResponse("INTEGRATION_USER_DETAILS", userInfo.error)
-  }
+    logger.info(
+      `request: ${JSON.stringify(
+        !userInfo.success ? userInfo.error.message : ""
+      )}`
+    )
 
-  if (!userInfo.data.customer?.allowAddInstance) {
-    return resolveErrorResponse("CUSTOMER_NOT_ALLOWED_ADD_INSTANCE")
-  }
-
-  const customerId = userInfo.data.customer?.id
-
-  if (customerId === undefined) {
-    return resolveErrorResponse("MISSING_CUSTOMER_ID")
-  }
-
-  /**
-   * Loose check to make sure the user is allowed to create an instance of the specified integration
-   *
-   * if an integration with the name shows up in their marketplace integration response then considered them allowed.
-   */
-  const integrationResp = await getMarketplaceIntegrationByName(
-    customerUrqlClient,
-    {
-      name: ALGOLIA_INTEGRATION_NAME,
+    if (didRequestFail(userInfo)) {
+      return resolveErrorResponse("INTEGRATION_USER_DETAILS", userInfo.error)
     }
-  )
 
-  logger.info(`getIntegration: ${integrationResp.success}`)
-
-  if (didRequestFail(integrationResp)) {
-    return resolveErrorResponse(
-      "INTEGRATION_GET_INTEGRATION",
-      integrationResp.error
-    )
-  }
-
-  if (!userInfo.data.customer?.externalId) {
-    return resolveErrorResponse(
-      "CREATE_INTEGRATION_INSTANCE",
-      new Error(`Missing external id for customer.`)
-    )
-  }
-
-  /**
-   * Get access token for system user
-   */
-  const systemUserAccessTokenResult = await getSystemAccessToken(
-    userInfo.data.customer.externalId
-  )
-
-  if (!systemUserAccessTokenResult.success) {
-    return resolveErrorResponse(
-      "CREATE_INTEGRATION_INSTANCE",
-      systemUserAccessTokenResult.error
-    )
-  }
-
-  const systemUrqlClient = createUrqlClient(
-    systemUserAccessTokenResult.token,
-    host
-  )
-
-  const epccBaseUrl = resolveEpccBaseUrl(host)
-
-  const createdInstanceResponse = await createIntegrationInstance(
-    systemUrqlClient,
-    {
-      integrationId: ALGOLIA_INTEGRATION_ID,
-      customerId,
-      name: ALGOLIA_INTEGRATION_NAME,
-      description: "Algolia Integration",
-      configVariables: createAlgoliaIntegrationConfig({
-        algoliaAppId: appId,
-        algoliaAdminApiKey: adminApiKey,
-        epccComponentConnectionShared: {
-          clientId,
-          clientSecret,
-          tokenUrl: `${epccBaseUrl}/oauth/access_token`,
-        },
-        webhookKey: createWebhookSecretKey(),
-        epccBaseUrl: `${epccBaseUrl}`,
-      }),
+    if (!userInfo.data.customer?.allowAddInstance) {
+      return resolveErrorResponse("CUSTOMER_NOT_ALLOWED_ADD_INSTANCE")
     }
-  )
 
-  if (didRequestFail(createdInstanceResponse)) {
-    return resolveErrorResponse(
-      "CREATE_INTEGRATION_INSTANCE",
-      createdInstanceResponse.error
+    const customerId = userInfo.data.customer?.id
+
+    if (customerId === undefined) {
+      return resolveErrorResponse("MISSING_CUSTOMER_ID")
+    }
+
+    /**
+     * Loose check to make sure the user is allowed to create an instance of the specified integration
+     *
+     * if an integration with the name shows up in their marketplace integration response then considered them allowed.
+     */
+    const integrationResp = await getMarketplaceIntegrationByName(
+      customerUrqlClient,
+      {
+        name: ALGOLIA_INTEGRATION_NAME,
+      }
     )
-  }
 
-  logger.info(
-    `response to create integration instance: ${JSON.stringify(
-      createdInstanceResponse.data
-    )}`
-  )
+    logger.info(`getIntegration: ${integrationResp.success}`)
 
-  return {
-    success: true,
-    name: "algolia",
-    result: createdInstanceResponse.data,
+    if (didRequestFail(integrationResp)) {
+      return resolveErrorResponse(
+        "INTEGRATION_GET_INTEGRATION",
+        integrationResp.error
+      )
+    }
+
+    if (!userInfo.data.customer?.externalId) {
+      return resolveErrorResponse(
+        "CREATE_INTEGRATION_INSTANCE",
+        new Error(`Missing external id for customer.`)
+      )
+    }
+
+    /**
+     * Get access token for system user
+     */
+    const systemUserAccessTokenResult = await getSystemAccessToken(
+      userInfo.data.customer.externalId
+    )
+
+    if (!systemUserAccessTokenResult.success) {
+      return resolveErrorResponse(
+        "CREATE_INTEGRATION_INSTANCE",
+        systemUserAccessTokenResult.error
+      )
+    }
+
+    const systemUrqlClient = createUrqlClient(
+      systemUserAccessTokenResult.token,
+      host
+    )
+
+    const epccBaseUrl = resolveEpccBaseUrl(host)
+
+    const createdInstanceResponse = await createIntegrationInstance(
+      systemUrqlClient,
+      {
+        integrationId: ALGOLIA_INTEGRATION_ID,
+        customerId,
+        name: ALGOLIA_INTEGRATION_NAME,
+        description: "Algolia Integration",
+        configVariables: createAlgoliaIntegrationConfig({
+          algoliaAppId: appId,
+          algoliaAdminApiKey: adminApiKey,
+          epccComponentConnectionShared: {
+            clientId,
+            clientSecret,
+            tokenUrl: `${epccBaseUrl}/oauth/access_token`,
+          },
+          webhookKey: createWebhookSecretKey(),
+          epccBaseUrl: `${epccBaseUrl}`,
+        }),
+      }
+    )
+
+    if (didRequestFail(createdInstanceResponse)) {
+      return resolveErrorResponse(
+        "CREATE_INTEGRATION_INSTANCE",
+        createdInstanceResponse.error
+      )
+    }
+
+    return {
+      success: true,
+      name: "algolia",
+      result: createdInstanceResponse.data,
+    }
+  } catch (err: unknown) {
+    return resolveErrorResponse(
+      "UNKNOWN",
+      err instanceof Error
+        ? err
+        : new Error(`Unknown error ${JSON.stringify(err)}`)
+    )
   }
 }
