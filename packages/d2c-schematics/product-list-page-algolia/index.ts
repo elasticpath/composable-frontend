86b8482e12e3cc3f19925c62cff83e30d3a985ed
Robert Field <robertfield@quadrimular.com>
2023-03-29 11:07:31 +0100

    fix: skip deps install when adding algolia deps

--- packages/d2c-schematics/product-list-page-algolia/index.ts
+++ packages/d2c-schematics/product-list-page-algolia/index.ts
@@ -51,6 +51,7 @@ export default function (options: AlgoliaProductListOptions): Rule {
           ? `${options.path}/package.json`
           : "/package.json",
         existing: "skip",
+        install: "none",
       })
     ),
     mergeWith(
