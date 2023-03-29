34d431479d2dfb0b657f14f1c767b43b2c3ce453
Robert Field <robertfield@quadrimular.com>
2023-03-29 12:14:30 +0100

    fix: avoid outputting any plp files if none selected

--- packages/d2c-schematics/product-list-page/index.ts
+++ packages/d2c-schematics/product-list-page/index.ts
@@ -46,22 +46,24 @@ export default function (options: ProductListOptions): Rule {
     directory,
   }
 
-  return chain([
-    mergeWith(
-      apply(url("./files"), [
-        options.skipTests
-          ? filter((path) => !path.endsWith(".spec.ts.template"))
-          : noop(),
-        applyTemplates({
-          utils: strings,
-          ...options,
-        }),
-        move(options.path || ""),
-      ]),
-      MergeStrategy.Overwrite
-    ),
-    plpType === "none" ? noop() : schematic(plpType, algoliaPlpOptions),
-  ])
+  return plpType === "none"
+    ? noop()
+    : chain([
+        mergeWith(
+          apply(url("./files"), [
+            options.skipTests
+              ? filter((path) => !path.endsWith(".spec.ts.template"))
+              : noop(),
+            applyTemplates({
+              utils: strings,
+              ...options,
+            }),
+            move(options.path || ""),
+          ]),
+          MergeStrategy.Overwrite
+        ),
+        schematic(plpType, algoliaPlpOptions),
+      ])
 }
 
 function resolvePlpType(type?: PlpType): SupportedPlpSchematics {
