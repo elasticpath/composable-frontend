f04e496438ab75e755102e501d6fe00acab0263d
Robert Field <robertfield@quadrimular.com>
2023-03-29 21:26:57 +0100

    feat: add algolia integrations for each region

--- packages/mason-common/src/integration-hub/constants.ts
+++ packages/mason-common/src/integration-hub/constants.ts
@@ -1,7 +1,12 @@
+import { EpccRegion } from "./integration-hub-services"
+
 export const ALGOLIA_INTEGRATION_NAME = "Algolia - Full / Delta / Large Catalog"
 
-export const ALGOLIA_INTEGRATION_ID =
-  "SW50ZWdyYXRpb246NjQ2Y2VhYjgtODgxMS00MDM3LWE5NjItZDYwMTczNTJkOTJi"
+export const ALGOLIA_INTEGRATION_ID: Record<EpccRegion, string> = {
+  "eu-west": "SW50ZWdyYXRpb246ODc4Y2ZiYjEtMzU4ZC00Y2QyLThkMmEtNjVhOTM1YTJkMmFi",
+  unknown: "SW50ZWdyYXRpb246NjQ2Y2VhYjgtODgxMS00MDM3LWE5NjItZDYwMTczNTJkOTJi",
+  "us-east": "SW50ZWdyYXRpb246ZTAyMzcyODgtZjRiYi00YTliLWI0YzMtMjQ4MGI2MTM0Nzlh",
+} as const
 
 export const MASON_INTEGRATION_HUB_DEPLOYER_URL =
   "https://mason-integration-hub-deployer.netlify.app/.netlify/functions"
