2c61ecdbcd5d5dd04f63a7473f5c486c4c0ae3ce
Robert Field <robertfield@quadrimular.com>
2023-04-17 18:40:44 +0100

    feat: latest example generated

---
+++ examples/algolia/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: ["e2e/**/*"],
+    coverage: {
+      provider: "istanbul",
+    },
+  },
+});
