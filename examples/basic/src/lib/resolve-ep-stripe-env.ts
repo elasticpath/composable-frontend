4767ef7b75615dc167c7d6125608d994c617c7eb
Robert Field <robertfield@quadrimular.com>
2023-04-20 14:27:29 +0100

    feat: fixed exposure of secrets in logs

--- examples/basic/src/lib/resolve-ep-stripe-env.ts
+++ examples/basic/src/lib/resolve-ep-stripe-env.ts
@@ -11,7 +11,7 @@ function retrieveStripePublishableKey(): string {
   const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
   if (!stripePublishableKey) {
     throw new Error(
-      `Failed to get stripe instance with stripePublishableKey: ${stripePublishableKey}\n Make sure you have set NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY`
+      `Failed to get stripe instance. Make sure you have set NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY`
     );
   }
   return stripePublishableKey;
@@ -21,7 +21,7 @@ function retrieveStripeAccountId(): string {
   const stripeAccountId = process.env.NEXT_PUBLIC_STRIPE_ACCOUNT_ID;
   if (!stripeAccountId) {
     throw new Error(
-      `Failed to get stripe instance with stripeAccountId: ${stripeAccountId}\n Make sure you have set NEXT_PUBLIC_STRIPE_ACCOUNT_ID`
+      `Failed to get stripe instance. Make sure you have set NEXT_PUBLIC_STRIPE_ACCOUNT_ID`
     );
   }
   return stripeAccountId;
