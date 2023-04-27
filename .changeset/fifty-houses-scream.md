d5204dd4420d04783259ec8081bb3c232b0be6e9
Robert Field <robertfield@quadrimular.com>
2023-04-27 16:35:17 +0100

    feat: handling algolia failure better (#38)
    * fix(algolia): should not hard fail if instance already exists
    
    * chore(changeset): added changeset

---
+++ .changeset/fifty-houses-scream.md
@@ -0,0 +1,5 @@
+---
+"@elasticpath/d2c-schematics": patch
+---
+
+If algolia instance already exists in the integration hub for the users store then setup is considered a success.
--- packages/d2c-schematics/setup-integration/index.ts
+++ packages/d2c-schematics/setup-integration/index.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics"
+import { noop, Rule, SchematicContext, Tree } from "@angular-devkit/schematics"
 import { Schema as SetupIntegrationOptions } from "./schema"
 import { createFallbackLogger } from "../utility/fallback-logger"
 import { performAlgoliaIntegrationSetup } from "../utility/integration-hub/perform-algolia-integration-setup"
@@ -6,13 +6,25 @@ import { performAlgoliaIntegrationSetup } from "../utility/integration-hub/perfo
 export default function (options: SetupIntegrationOptions): Rule {
   return async (host: Tree, context: SchematicContext): Promise<Rule> => {
     const logger = context.logger ?? createFallbackLogger()
-    switch (options.integrationName) {
-      case "algolia":
-        return performAlgoliaIntegrationSetup(options, host, logger)
-      default:
-        return Promise.reject(
-          `Failed to setup integration, unsupported integration name: ${options.integrationName}`
-        )
+    try {
+      switch (options.integrationName) {
+        case "algolia":
+          return performAlgoliaIntegrationSetup(options, host, logger)
+        default:
+          logger.error(
+            `Failed to setup integration, unsupported integration name: ${options.integrationName}`
+          )
+          return noop()
+      }
+    } catch (err) {
+      logger.error(
+        `Failed to setup integration, unexpected error occurred: ${
+          err instanceof Error
+            ? `${err.name} - ${err.message}`
+            : "Couldn't process error message"
+        }`
+      )
+      return noop()
     }
   }
 }
