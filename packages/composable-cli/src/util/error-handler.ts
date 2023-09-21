341905336fec139b0043e6fcb705bfc1eeb6ee64
Robert Field <robertfield@quadrimular.com>
2023-09-21 11:23:41 +0100

    feat: attempt to surface some error messages

--- packages/composable-cli/src/util/error-handler.ts
+++ packages/composable-cli/src/util/error-handler.ts
@@ -1,9 +1,14 @@
 const makeErrorWrapper =
-  <T>(errorHandler: (err: any) => T) =>
-  <A extends any[], R>(fn: (...a: A) => Promise<R>) =>
+  <T>(errorHandler: (err: unknown) => T) =>
+  <A extends any[], R extends object>(fn: (...a: A) => Promise<R>) =>
   async (...a: A): Promise<void> => {
     try {
-      await fn(...a)
+      const result = await fn(...a)
+
+      if (isResultError(result)) {
+        return Promise.reject(result.error)
+      }
+
       return Promise.resolve()
     } catch (err) {
       await errorHandler(err)
@@ -11,7 +16,25 @@ const makeErrorWrapper =
     }
   }
 
-export const handleErrors = makeErrorWrapper(() => {
+function isResultError(
+  result: any
+): result is { success: false; error: unknown } {
+  return (
+    !!result &&
+    "success" in result &&
+    typeof result.success === "boolean" &&
+    !result.success
+  )
+}
+
+export const handleErrors = makeErrorWrapper((err) => {
+  if (err instanceof Error) {
+    console.error(err.name)
+    console.error(err.message)
+    console.error(err.stack)
+    console.error(err.cause)
+    return Promise.resolve()
+  }
   console.error("There was an unexpected error!")
   return Promise.resolve()
 })
