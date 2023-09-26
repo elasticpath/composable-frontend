cccb4fadffea994405bc85221bd81ee5741c42a9
Robert Field <robertfield@quadrimular.com>
2023-09-26 16:41:45 +0100

    feat: top level fail false to prevent all throw errors being processed in yargs

--- packages/composable-cli/src/composable.ts
+++ packages/composable-cli/src/composable.ts
@@ -65,6 +65,7 @@ export async function main({
       .example("$0 logout", "logout of the CLI")
       .strictCommands()
       .demandCommand(1)
+      .fail(false)
       .help("h").argv
 
     return 0
