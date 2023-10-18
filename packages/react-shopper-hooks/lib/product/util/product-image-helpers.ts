3b80e28d841564ad1dea4ced7998a77416ef2e08
Robert Field <robertfield@quadrimular.com>
2023-10-18 15:21:01 +0200

    fix: handle other images correctly

--- packages/react-shopper-hooks/lib/product/util/product-image-helpers.ts
+++ packages/react-shopper-hooks/lib/product/util/product-image-helpers.ts
@@ -9,19 +9,27 @@ export function processImageFiles(files: File[], mainImageId?: string) {
     "image/webp",
     "image/svg+xml",
   ]
+
+  if (!mainImageId) {
+    return files
+  }
+
   return files.filter(
     (fileEntry) =>
       fileEntry.id !== mainImageId &&
-      supportedMimeTypes.some((type) => fileEntry.mime_type === type)
+      supportedMimeTypes.some((type) => fileEntry.mime_type === type),
   )
 }
 
-export function getProductOtherImageUrls(files: File[] | undefined): File[] {
-  return files ? processImageFiles(files, files?.[0].id) : []
+export function getProductOtherImageUrls(
+  files: File[] | undefined,
+  mainImageFile: File | undefined,
+): File[] {
+  return files ? processImageFiles(files, mainImageFile?.id) : []
 }
 
 export function getProductMainImage(
-  mainImages: File[] | undefined
+  mainImages: File[] | undefined,
 ): File | null {
   return mainImages?.[0] || null
 }
