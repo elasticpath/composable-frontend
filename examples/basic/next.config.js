b62ab03d6b24c0dd60693204cc5b74602293f9d5
Robert Field <robertfield@quadrimular.com>
2023-10-30 11:27:02 +0100

    feat: bumped to next 14

--- examples/basic/next.config.js
+++ examples/basic/next.config.js
@@ -4,9 +4,6 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
