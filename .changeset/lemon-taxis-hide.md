3a6cdf33a860c94fb22319d8b6a8f2830c7c2404
Robert Field <robertfield@quadrimular.com>
2024-02-05 11:13:09 +0000

    Feat/show errors verbose (#186)
    * feat: set verbose argument properly on the command context
    
    * feat: login command updates
    
    - checks if user is authenticated, prompts if user wants to continue
    - logout will show an info message if not logged in
    
    * chore: changeset

---
+++ .changeset/lemon-taxis-hide.md
@@ -0,0 +1,6 @@
+---
+"composable-cli": patch
+---
+
+- checks if user is authenticated, prompts if user wants to continue
+- logout will show an info message if not logged in
--- packages/composable-cli/src/util/conf-store/store-credentials.ts
+++ packages/composable-cli/src/util/conf-store/store-credentials.ts
@@ -1,7 +1,11 @@
 import Conf from "conf"
-import { UserProfile } from "../../lib/epcc-user-profile-schema"
+import {
+  epccUserProfileSchema,
+  UserProfile,
+} from "../../lib/epcc-user-profile-schema"
 import { Credentials } from "../../lib/authentication/credentials-schema"
 import { UserStore } from "../../lib/stores/stores-schema"
+import { Result } from "../../types/results"
 
 export function storeCredentials(store: Conf, credentials: Credentials) {
   return store.set("credentials", credentials)
@@ -20,4 +24,31 @@ export function storeUserProfile(store: Conf, userProfile: UserProfile) {
 
 export function storeUserStore(store: Conf, userStore: UserStore) {
   return store.set("store", userStore)
+}
+
+export function getUserProfile(store: Conf): Result<UserProfile, Error> {
+  const possibleProfile = store.get("profile")
+
+  if (!possibleProfile) {
+    return {
+      success: false,
+      error: new Error("No profile found in store"),
+    }
+  }
+
+  const parsedProfile = epccUserProfileSchema.safeParse(possibleProfile)
+
+  if (!parsedProfile.success) {
+    return {
+      success: false,
+      error: new Error(
+        `Profile failed to parse from store - ${parsedProfile.error.errors.toString()}`,
+      ),
+    }
+  }
+
+  return {
+    success: true,
+    data: parsedProfile.data,
+  }
 }
