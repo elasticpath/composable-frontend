a0b92866f237daa3f8318e6b2378c2e564a29419
Robert Field <robertfield@quadrimular.com>
2024-05-01 11:31:22 +0100

    feat: add product list example

--- packages/react-shopper-hooks/example/App.tsx
+++ packages/react-shopper-hooks/example/App.tsx
@@ -4,27 +4,48 @@ import CartExample from "./CartExample"
 import { CartProvider } from "../src/cart"
 import { gateway as EPCCGateway } from "@moltin/sdk"
 import { StoreProvider } from "../src/store"
+import { ElasticPathProvider } from "../src"
+import { QueryClient } from "@tanstack/react-query"
+import { ProductListExample } from "./ProductListExample"
+
+const client = EPCCGateway({
+  name: "my_store",
+  client_id: import.meta.env.VITE_APP_EPCC_CLIENT_ID,
+  client_secret: import.meta.env.VITE_APP_EPCC_CLIENT_SECRET,
+  host: import.meta.env.VITE_APP_EPCC_HOST,
+})
+
+const queryClient = new QueryClient()
 
 function App() {
-  const client = EPCCGateway({
-    name: "my_store",
-    client_id: import.meta.env.VITE_APP_EPCC_CLIENT_ID,
-    client_secret: import.meta.env.VITE_APP_EPCC_CLIENT_SECRET,
-    host: import.meta.env.VITE_APP_EPCC_HOST,
-  })
+  const [activeItem, setActiveItem] = React.useState<"cart" | "products">(
+    "cart",
+  )
 
   return (
     <div className="App">
       <h1>React Shopper Hooks</h1>
+      <div>
+        <button onClick={() => setActiveItem("cart")}>Cart View</button>
+        <button onClick={() => setActiveItem("products")}>Products View</button>
+      </div>
       <div className="card">
-        <StoreProvider cartId={client.Cart().cartId}>
-          <CartProvider cartId={client.Cart().cartId}>
-            <CartExample />
-          </CartProvider>
-        </StoreProvider>
+        <ElasticPathProvider
+          client={client}
+          queryClientProviderProps={{ client: queryClient }}
+        >
+          <StoreProvider cartId={client.Cart().cartId}>
+            <CartProvider cartId={client.Cart().cartId}>
+              {activeItem === "cart" && <CartExample />}
+              {activeItem === "products" && <ProductListExample />}
+            </CartProvider>
+          </StoreProvider>
+        </ElasticPathProvider>
       </div>
     </div>
   )
 }
+
+function TanstackWrapper() {}
 
 export default App
