2f8c63e36f4528c96f17f31a810b94740b261c8e
Robert Field <robertfield@quadrimular.com>
2024-05-07 13:50:42 +0100

    feat: removed cart provider

--- packages/react-shopper-hooks/src/store/store-provider.tsx
+++ packages/react-shopper-hooks/src/store/store-provider.tsx
@@ -1,6 +1,4 @@
 import { StoreProviderProps } from "./types/store-context-types"
-import { emitter } from "../event/event-context"
-import { CartProvider } from "../cart"
 import React, { createContext } from "react"
 import type { Moltin as EPCCClient } from "@moltin/sdk"
 import { NavigationNode } from "@elasticpath/shopper-common"
@@ -16,21 +14,12 @@ export const StoreProviderContext = createContext<StoreState | null>(null)
 export const StoreProvider = ({
   children,
   initialState,
-  cartId,
 }: StoreProviderProps) => {
   const { client } = useElasticPath()
 
   return (
     <StoreProviderContext.Provider value={{ client, nav: initialState?.nav }}>
-      <CartProvider
-        cartId={cartId}
-        initialState={{
-          cart: initialState?.cart,
-        }}
-        emit={emitter}
-      >
-        {children}
-      </CartProvider>
+      {children}
     </StoreProviderContext.Provider>
   )
 }
