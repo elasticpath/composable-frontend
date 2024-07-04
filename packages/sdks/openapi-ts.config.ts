696382f99459b84a0ed6d19b44e55313d3a09295
Robert Field <robertfield@quadrimular.com>
2024-07-04 16:47:16 +0100

    feat: bumped latest

--- packages/sdks/openapi-ts.config.ts
+++ packages/sdks/openapi-ts.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
+  client: "@hey-api/client-fetch",
   input: "./specs/pim.yaml",
-  output: "src/client",
+  output: { path: "src/client", format: "prettier" },
+  types: {
+    name: "PascalCase",
+    enums: false,
+  },
 })
