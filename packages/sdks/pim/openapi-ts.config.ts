93ae2f005533c2f03469fa992391548da6fc7f48
Robert Field <robertfield@quadrimular.com>
2024-08-13 19:54:33 +0100

    refactor: move pim sdk

--- packages/sdks/pim/openapi-ts.config.ts
+++ packages/sdks/pim/openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
   client: "@hey-api/client-fetch",
-  input: "./specs/pim.yaml",
+  input: "../specs/pim.yaml",
   output: { path: "src/client", format: "prettier" },
   types: {
     name: "PascalCase",
