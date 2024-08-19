7e1eb2c80c0747a1a00f09e884fbea292d9ade1c
Robert Field <robertfield@quadrimular.com>
2024-08-19 12:04:47 +0100

    feat: remove service layer use sdk directly

--- examples/simple/src/services/products.ts
+++ examples/simple/src/services/products.ts
@@ -1,12 +1,4 @@
-import type { ProductResponse, ResourcePage } from "@elasticpath/js-sdk";
-import { wait300 } from "../lib/product-helper";
 import { ElasticPath } from "@elasticpath/js-sdk";
-
-export function getAllProducts(
-  client: ElasticPath,
-): Promise<ProductResponse[]> {
-  return _getAllProductPages(client)();
-}
 
 export function getProducts(client: ElasticPath, offset = 0, limit = 100) {
   return client.ShopperCatalog.Products.With(["main_image"])
@@ -14,40 +6,3 @@ export function getProducts(client: ElasticPath, offset = 0, limit = 100) {
     .Offset(offset)
     .All();
 }
-
-const _getAllPages =
-  <T, I>(
-    nextPageRequestFn: (
-      limit: number,
-      offset: number,
-      client?: ElasticPath,
-    ) => Promise<ResourcePage<T, I>>,
-  ) =>
-  async (
-    offset: number = 0,
-    limit: number = 25,
-    accdata: T[] = [],
-  ): Promise<T[]> => {
-    const requestResp = await nextPageRequestFn(limit, offset);
-    const {
-      meta: {
-        page: newPage,
-        results: { total },
-      },
-      data: newData,
-    } = requestResp;
-
-    const updatedOffset = offset + newPage.total;
-    const combinedData = [...accdata, ...newData];
-    if (updatedOffset < total) {
-      return wait300.then(() =>
-        _getAllPages(nextPageRequestFn)(updatedOffset, limit, combinedData),
-      );
-    }
-    return Promise.resolve(combinedData);
-  };
-
-const _getAllProductPages = (client: ElasticPath) =>
-  _getAllPages((limit = 25, offset = 0) =>
-    client.ShopperCatalog.Products.Limit(limit).Offset(offset).All(),
-  );
