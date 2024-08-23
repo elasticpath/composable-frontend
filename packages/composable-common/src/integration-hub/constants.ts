1a3fd3b58b1254b8acbe95146fc40ac18f046848
Robert Field <robertfield@quadrimular.com>
2024-08-23 18:13:01 +0100

    feat: update algolia integration ids

--- packages/composable-common/src/integration-hub/constants.ts
+++ packages/composable-common/src/integration-hub/constants.ts
@@ -4,9 +4,9 @@ export const ALGOLIA_INTEGRATION_NAME = "Algolia - Full / Delta / Large Catalog"
 export const KLEVU_INTEGRATION_NAME = "Klevu Catalog Sync"
 
 export const ALGOLIA_INTEGRATION_ID: Record<EpccRegion, string> = {
-  "eu-west": "SW50ZWdyYXRpb246ODc4Y2ZiYjEtMzU4ZC00Y2QyLThkMmEtNjVhOTM1YTJkMmFi",
-  unknown: "SW50ZWdyYXRpb246NjQ2Y2VhYjgtODgxMS00MDM3LWE5NjItZDYwMTczNTJkOTJi",
-  "us-east": "SW50ZWdyYXRpb246ZTAyMzcyODgtZjRiYi00YTliLWI0YzMtMjQ4MGI2MTM0Nzlh",
+  "eu-west": "SW50ZWdyYXRpb246NTAyMTVmODYtZDgzYi00NDUxLWE5ODktMzIyNmQ5NjQzY2Iw",
+  unknown: "SW50ZWdyYXRpb246ZTFmOTM2ZmUtMmE5MC00ODQxLWJhYjAtNTAzMjZkNWQ5OTI4",
+  "us-east": "SW50ZWdyYXRpb246M2JmMjAwNTEtYjlhYy00MTQ3LWE3ODYtNGFmODNlOTcyZmE3",
 } as const
 
 export const KLEVU_INTEGRATION_ID: Record<EpccRegion, string> = {
