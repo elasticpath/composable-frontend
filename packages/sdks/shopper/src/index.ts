a4a31a3d53f2523dee204dd8217a278a163851cd
Robert Field <robertfield@quadrimular.com>
2025-01-09 18:11:34 +0000

    feat: export tanstack options

--- packages/sdks/shopper/src/index.ts
+++ packages/sdks/shopper/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./client"
+export * from "./client/@tanstack/react-query.gen"
 import { Client, createClient } from "@hey-api/client-fetch"
 export { createClient, Client }
 export { client } from "./client/sdk.gen"
