cf80da889a3912d44a4180bc2a1a04f3b401926b
Robert Field <robertfield@quadrimular.com>
2025-01-10 15:59:04 +0000

    feat: using PascalCase for types correctly

--- packages/sdks/pim/openapi-ts.config.ts
+++ packages/sdks/pim/openapi-ts.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "@hey-api/openapi-ts"
+import { defaultPlugins, defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
   client: "@hey-api/client-fetch",
   input: "../specs/pim.yaml",
   output: { path: "src/client", format: "prettier" },
-  types: {
-    name: "PascalCase",
-    enums: false,
-    dates: "types+transform",
-  },
+  plugins: [
+    ...defaultPlugins,
+    {
+      style: "PascalCase",
+      exportInlineEnums: false,
+      name: "@hey-api/typescript",
+    },
+    {
+      dates: true,
+      name: "@hey-api/transformers",
+    },
+  ],
 })
