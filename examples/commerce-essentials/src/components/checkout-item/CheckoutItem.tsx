04270bf525296010e3ac1d27c0b1f332d4c140d3
jimmyventuragmail <jimmy.ventura@gmail.com>
2025-01-24 10:26:26 -0500

    added checkoutitem component

--- examples/commerce-essentials/src/components/checkout-item/CheckoutItem.tsx
+++ examples/commerce-essentials/src/components/checkout-item/CheckoutItem.tsx
@@ -2,15 +2,17 @@
 import { ProductThumbnail } from "../../app/(store)/account/orders/[orderId]/ProductThumbnail";
 import Link from "next/link";
 import { CartItem } from "@elasticpath/js-sdk";
+import { getProductURLSegment } from "../../lib/product-helper";
 
-export function CheckoutItem({ item }: { item: CartItem }) {
+export function CheckoutItem({ item, productSlug }: { item: CartItem, productSlug?: string }) {
+  const canonicalURL = getProductURLSegment({ id: item.product_id, attributes: { slug: productSlug } });
   return (
     <div className="flex w-full lg:w-[24.375rem] gap-5 items-start">
       <div className="flex flex-col w-[4.5rem] h-[5.626rem] justify-start shrink-0 items-center">
         <ProductThumbnail productId={item.product_id} />
       </div>
       <div className="flex flex-col items-start gap-2.5 flex-only-grow">
-        <Link href={`/products/${item.product_id}`}>
+        <Link href={canonicalURL}>
           <span className="font-medium text-xl">{item.name}</span>
         </Link>
         <span className="text-sm text-black/60">Quantity: {item.quantity}</span>
