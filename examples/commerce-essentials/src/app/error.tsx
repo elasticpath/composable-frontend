f472032b89890191ed0a645e10e26c561f6e0b6a
Jimmy Isaac Ventura Salcedo <jimmy.ventura@elasticpath.com>
2025-01-31 11:10:34 -0500

    restored error page to main branch

--- examples/commerce-essentials/src/app/error.tsx
+++ examples/commerce-essentials/src/app/error.tsx
@@ -14,11 +14,6 @@ export default function GlobalError({
         <div className="flex h-[36rem] flex-col items-center justify-center gap-4 p-8">
           <span className="text-center text-xl md:text-3xl">
             {error.digest} - Internal server error.
-            
-          </span>
-          <span>
-          Stacktrace: 
-          {error.stack}
           </span>
           <Link href="/" className="font-md md:font-lg text-brand-primary">
             Back to home
