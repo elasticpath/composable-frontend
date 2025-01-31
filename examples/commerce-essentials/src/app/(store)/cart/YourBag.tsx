0c2046004d51e021654c2553cd96b06884b207f7
Jimmy Isaac Ventura Salcedo <jimmy.ventura@elasticpath.com>
2025-01-31 11:17:38 -0500

    Silly me, the cart item has the slug unlike the order item

--- examples/commerce-essentials/src/app/(store)/cart/YourBag.tsx
+++ examples/commerce-essentials/src/app/(store)/cart/YourBag.tsx
@@ -5,12 +5,8 @@ import { useCart, useProducts } from "@elasticpath/react-shopper-hooks";
 
 export function YourBag() {
   const { data } = useCart();
-  const productSlugMap = new Map<string, string>();
   const state = data?.state;
-  const productResult = useProducts({ filter: { in: { id: state?.items.map(cartItem => cartItem.product_id) } } }, { enabled: state?.items && state?.items?.length > 0, });
-  productResult?.data?.data?.forEach((product) => {
-    productSlugMap.set(product.id, product.attributes.slug)
-  });
+
   return (
     <ul role="list" className="flex flex-col items-start gap-5 self-stretch">
       {state?.items.map((item) => {
@@ -19,7 +15,7 @@ export function YourBag() {
             key={item.id}
             className="self-stretch border-t border-zinc-300 py-5"
           >
-            <CartItemWide item={item} productSlug={productSlugMap.get(item.product_id)} />
+            <CartItemWide item={item} productSlug={item.slug} />
           </li>
         );
       })}
