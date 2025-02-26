006cd87ebe7c81320051d508332566c57643fd18
Anthony McAffee <anthony.mcaffee@elasticpath.com>
2025-02-26 10:08:33 -0400

    Attempt to resolve canonical domain.

--- examples/commerce-essentials/src/app/(store)/products/[...productSegment]/page.tsx
+++ examples/commerce-essentials/src/app/(store)/products/[...productSegment]/page.tsx
@@ -21,8 +21,8 @@ type Props = {
   params: { productId: string };
 };
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const baseUrl = process.env.NEXT_PUBLIC_DOMAIN_NAME
+  ? `https://${process.env.NEXT_PUBLIC_DOMAIN_NAME}`
   : "http://localhost:3000";
 
 export async function generateMetadata({
