720e1b91963f48ca9a1ffdeadfbb6dc73322d7e5
Robert Field <robertfield@quadrimular.com>
2025-03-07 14:10:03 +0000

    feat: add missing sdks (#309)
    * refactor: remove log
    
    * feat: add specs and base sdks
    
    * feat: build application keys
    
    * feat: build commerce extensions keys
    
    * feat: build flows
    
    * feat: build integrations
    
    * feat: build payments
    
    * feat: build permissions
    
    * feat: build promotions standard
    
    * feat: build rule promotions
    
    * feat: build settings

---
+++ packages/sdks/flows/openapi-ts.config.ts
@@ -0,0 +1,24 @@
+import { defaultPlugins, defineConfig } from "@hey-api/openapi-ts"
+import { defineConfig as defineReadmeConfig } from "../specs/heyapi/plugins"
+
+export default defineConfig({
+  client: "@hey-api/client-fetch",
+  experimentalParser: true,
+  input: "../specs/flows.yaml",
+  output: { path: "src/client", format: "prettier" },
+  plugins: [
+    ...defaultPlugins,
+    {
+      exportInlineEnums: true,
+      name: "@hey-api/typescript",
+    },
+    {
+      dates: true,
+      name: "@hey-api/transformers",
+    },
+    defineReadmeConfig({
+      name: "generate-readme",
+      targetOperation: "getAFlow",
+    }),
+  ],
+})
