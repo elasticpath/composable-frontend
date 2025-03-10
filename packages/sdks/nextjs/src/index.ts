da62157a671a39e54a73f93e326e0933ed502dbd
Robert Field <robertfield@quadrimular.com>
2025-03-10 13:53:00 +0000

    feat: add nextjs account utils (#345)

--- packages/sdks/nextjs/src/index.ts
+++ packages/sdks/nextjs/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./interceptors/auth-cookie-interceptor"
 export { getCookieValue } from "./util/get-cookie-value"
+export { getAccountCookie } from "./util/get-account-cookie"
+export { isAccountAuthenticated } from "./util/is-account-authenticated"
---
+++ packages/sdks/nextjs/src/util/get-account-cookie.ts
@@ -0,0 +1,21 @@
+import { getCookieValue } from "./get-cookie-value"
+
+export const DEFAULT_ACCOUNT_MEMBER_TOKEN_COOKIE_NAME =
+  "`_store_ep_account_member_token`;"
+
+export async function getAccountCookie(
+  key: string = DEFAULT_ACCOUNT_MEMBER_TOKEN_COOKIE_NAME,
+) {
+  let cookieValue: string | undefined
+
+  if (typeof window === "undefined") {
+    // Dynamically import next/headers on the server.
+    const headersModule = await import("next/headers")
+    cookieValue = (await headersModule.cookies()).get(key)?.value
+  } else {
+    // Client side: read document.cookie.
+    cookieValue = getCookieValue(key)
+  }
+
+  return cookieValue
+}
