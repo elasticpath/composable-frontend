8f5db8be623463af05cf843af746229d56cf8313
Robert Field <robertfield@quadrimular.com>
2025-05-07 16:04:56 +0100

    feat: working local storage example

--- examples/authentication-local-storage/src/app/client-component.tsx
+++ examples/authentication-local-storage/src/app/client-component.tsx
@@ -2,61 +2,43 @@
 
 import { useEffect, useState } from "react"
 import {
-  AccessTokenResponse,
-  client,
   getByContextAllProducts,
   ProductListData,
 } from "@epcc-sdk/sdks-shopper"
-import { CREDENTIALS_COOKIE_KEY } from "./constants"
-
-client.setConfig({
-  baseUrl: process.env.NEXT_PUBLIC_EPCC_ENDPOINT_URL!,
-})
-
-client.interceptors.request.use((request, options) => {
-  const credentials = JSON.parse(
-    localStorage.getItem(CREDENTIALS_COOKIE_KEY) ?? "{}",
-  ) as AccessTokenResponse | undefined
-  if (credentials) {
-    request.headers.set("Authorization", `Bearer ${credentials.access_token}`)
-  }
-  return request
-})
 
 export function ClientComponent() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [products, setProducts] = useState<ProductListData["data"]>([])
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const fetchProducts = async () => {
+    const response = await getByContextAllProducts()
+    setProducts(response.data?.data || [])
+    if (response.data?.data && response.data?.data.length > 0) {
+      setIsAuthenticated(true)
+    }
+  }
 
   useEffect(() => {
-    // fetch products using sdk
-    const fetchProducts = async () => {
-      console.log("fetching products")
-      const response = await getByContextAllProducts()
-      setProducts(response.data?.data || [])
-      console.log("response", response)
-
-      setIsAuthenticated(
-        (response.data?.data && response.data.data.length > 0) || false,
-      )
-    }
     fetchProducts()
   }, [])
+
+  // fetch products using sdk
 
   return (
     <>
       <div className="mb-6 border-b border-gray-300 pb-3">
         <h1 className="text-xl font-medium mb-2 text-black">
-          Authentication Demo
+          Storefront Authentication Demo
         </h1>
         <p className="text-black">
           Status:{" "}
           {isAuthenticated ? (
             <span className="font-semibold text-green-800">
-              Store successfully authenticated
+              Storefront successfully authenticated
             </span>
           ) : (
             <span className="font-semibold text-red-800">
-              Not authenticated
+              Storefront not authenticated
             </span>
           )}
         </p>
--- examples/authentication-local-storage/src/app/layout.tsx
+++ examples/authentication-local-storage/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import { AccessTokenResponse, client } from "@epcc-sdk/sdks-shopper"
-import { CREDENTIALS_COOKIE_KEY } from "./constants"
+import { StorefrontProvider } from "./auth/StorefrontProvider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <StorefrontProvider>{children}</StorefrontProvider>
       </body>
     </html>
   )
