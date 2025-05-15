dbf045719531ed84325f124fc2fd80f0586a6c8d
Robert Field <robertfield@quadrimular.com>
2025-05-15 16:36:52 +0100

    feat: working pwa example

--- examples/pwa-authentication/src/main.tsx
+++ examples/pwa-authentication/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { StorefrontProvider } from "./auth/StorefrontProvider.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <StorefrontProvider>
+      <App />
+    </StorefrontProvider>
+  </React.StrictMode>,
 )
