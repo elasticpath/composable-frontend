87c68d99932d68b18cc3bd94fc0a6559773d4880
Robert Field <robertfield@quadrimular.com>
2025-05-21 16:18:08 +0100

    feat: add credentials interceptor

---
+++ packages/sdks/shopper/src/constants/crendentials.ts
@@ -0,0 +1,2 @@
+// TODO remove _store prefix once work has been done in examples folder
+export const CREDENTIALS_STORAGE_KEY = "_store_ep_credentials"
---
+++ packages/sdks/shopper/src/interceptors/auth-local-storage-interceptor.ts
@@ -0,0 +1,63 @@
+import { Client } from "@hey-api/client-fetch"
+import { CREDENTIALS_STORAGE_KEY } from "../constants/crendentials"
+import { AccessTokenResponse, createAnAccessToken } from "../client"
+import { tokenExpired } from "../utils/token-expired"
+
+export function createAuthLocalStorageInterceptor(createOptions: {
+  storageKey?: string
+  clientId: string
+  autoRefresh?: boolean
+  autoStoreCredentials?: boolean
+}): Parameters<Client["interceptors"]["request"]["use"]>[0] {
+  return async function authCookieInterceptor(request, _options) {
+    const {
+      storageKey = CREDENTIALS_STORAGE_KEY,
+      clientId,
+      autoRefresh = true,
+      autoStoreCredentials = true,
+    } = createOptions
+
+    // Bypass interceptor logic for token requests to prevent infinite loop
+    if (request.url?.includes("/oauth/access_token")) {
+      return request
+    }
+
+    let credentials = JSON.parse(localStorage.getItem(storageKey) ?? "{}") as
+      | AccessTokenResponse
+      | undefined
+
+    // Create a new token if...
+    if (
+      // We don't have a token and auto-storing is enabled
+      (!credentials?.access_token && autoStoreCredentials) ||
+      // OR token exists but is expired and auto-refresh is enabled
+      (credentials?.access_token &&
+        credentials.expires &&
+        tokenExpired(credentials.expires) &&
+        autoRefresh)
+    ) {
+      if (!clientId) {
+        throw new Error("Missing storefront client id")
+      }
+      const authResponse = await createAnAccessToken({
+        body: {
+          grant_type: "implicit",
+          client_id: clientId,
+        },
+      })
+
+      if (!authResponse.data) {
+        throw new Error("Failed to get access token")
+      }
+
+      credentials = authResponse.data
+      // Store the credentials in localStorage
+      localStorage.setItem(storageKey, JSON.stringify(credentials))
+    }
+
+    if (credentials?.access_token) {
+      request.headers.set("Authorization", `Bearer ${credentials.access_token}`)
+    }
+    return request
+  }
+}
