f036d0c8a6b4281c04598ecfc4d2294bc72389e0
Robert Field <robertfield@quadrimular.com>
2025-05-23 16:12:50 +0100

    feat: update provider to use new interceptor

--- examples/spa-cart/src/auth/StorefrontProvider.tsx
+++ examples/spa-cart/src/auth/StorefrontProvider.tsx
@@ -1,16 +1,10 @@
 "use client"
 
-import React, { useCallback, useEffect } from "react"
+import React, { useEffect } from "react"
 import {
-  type AccessTokenResponse,
   client,
-  createAnAccessToken,
+  createAuthLocalStorageInterceptor,
 } from "@epcc-sdk/sdks-shopper"
-import { CREDENTIALS_COOKIE_KEY } from "../constants"
-
-function tokenExpired(expires: number): boolean {
-  return Math.floor(Date.now() / 1000) >= expires
-}
 
 client.setConfig({
   baseUrl: import.meta.env.VITE_APP_EPCC_ENDPOINT_URL!,
@@ -21,60 +15,15 @@ export function StorefrontProvider({
 }: {
   children: React.ReactNode
 }) {
-  const interceptor: Parameters<typeof client.interceptors.request.use>[0] =
-    useCallback(async (request) => {
-      // Bypass interceptor logic for token requests to prevent infinite loop
-      if (request.url?.includes("/oauth/access_token")) {
-        return request
-      }
-
-      let credentials = JSON.parse(
-        localStorage.getItem(CREDENTIALS_COOKIE_KEY) ?? "{}",
-      ) as AccessTokenResponse | undefined
-
-      // check if token expired or missing
-      if (
-        !credentials?.access_token ||
-        (credentials.expires && tokenExpired(credentials.expires))
-      ) {
-        const clientId = import.meta.env.VITE_APP_EPCC_CLIENT_ID
-
-        if (!clientId) {
-          throw new Error("Missing storefront client id")
-        }
-
-        const authResponse = await createAnAccessToken({
-          body: {
-            grant_type: "implicit",
-            client_id: clientId,
-          },
-        })
-
-        /**
-         * Check response did not fail
-         */
-        if (!authResponse.data) {
-          throw new Error("Failed to get access token")
-        }
-
-        const token = authResponse.data
-
-        // Store the credentials in localStorage
-        localStorage.setItem(CREDENTIALS_COOKIE_KEY, JSON.stringify(token))
-        credentials = token
-      }
-
-      if (credentials?.access_token) {
-        request.headers.set(
-          "Authorization",
-          `Bearer ${credentials.access_token}`,
-        )
-      }
-      return request
-    }, [])
-
-  // Auto-authenticate if not already authenticated
   useEffect(() => {
+    if (!import.meta.env.VITE_APP_EPCC_CLIENT_ID) {
+      throw new Error("Missing storefront client id")
+    }
+
+    const interceptor = createAuthLocalStorageInterceptor({
+      clientId: import.meta.env.VITE_APP_EPCC_CLIENT_ID,
+    })
+
     // Add request interceptor to include the token in requests
     client.interceptors.request.use(interceptor)
 
