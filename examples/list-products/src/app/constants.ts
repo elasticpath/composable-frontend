11a81294bab53d6b59b62fa6601c75cb09edb2f0
Robert Field <robertfield@quadrimular.com>
2025-06-12 13:06:39 +0100

    feat: init adding of list product example

---
+++ examples/list-products/src/app/constants.ts
@@ -0,0 +1,3 @@
+export const EPCC_ENDPOINT_URL = process.env.NEXT_PUBLIC_EPCC_ENDPOINT_URL
+export const COOKIE_PREFIX_KEY = "_store"
+export const CREDENTIALS_COOKIE_KEY = `${COOKIE_PREFIX_KEY}_ep_credentials`
