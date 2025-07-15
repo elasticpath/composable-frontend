36f3b7b4703f0aafa7116be8c6781f3681d47a6b
Sam Blacklock <sam.blacklock@elasticpath.com>
2025-07-15 15:31:04 +0100

    feat: clear unintentional commit

--- examples/next-account-checkout/src/app/(auth)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react"
-
-export default function AuthLayout({ children }: { children: ReactNode }) {
-  return <div className="bg-gray-50 min-h-screen">{children}</div>
-}
--- examples/next-account-checkout/src/app/constants.ts
+++
@@ -1,4 +0,0 @@
-export const EPCC_ENDPOINT_URL = process.env.NEXT_PUBLIC_EPCC_ENDPOINT_URL
-export const COOKIE_PREFIX_KEY = "_store"
-export const CREDENTIALS_COOKIE_KEY = `${COOKIE_PREFIX_KEY}_ep_credentials`
-export const ACCOUNT_MEMBER_TOKEN_COOKIE_NAME = `${COOKIE_PREFIX_KEY}_account_member_token`
