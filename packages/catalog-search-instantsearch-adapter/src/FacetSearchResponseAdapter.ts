c8c10cb863726ad225dad2f4b1be84c97f569bb3
Robert Field <robertfield@quadrimular.com>
2025-08-21 10:24:03 +0100

    feat: first pass working

--- packages/catalog-search-instantsearch-adapter/src/FacetSearchResponseAdapter.ts
+++ packages/catalog-search-instantsearch-adapter/src/FacetSearchResponseAdapter.ts
@@ -1,81 +1,95 @@
-"use strict";
+"use strict"
 
-import { utils } from "./support/utils";
+import { utils } from "./support/utils"
+import type { SearchClient } from "algoliasearch-helper/types/algoliasearch"
+import { SearchResult } from "@epcc-sdk/sdks-shopper"
+
+// InstantSearch.js sends requests in this format
+// interface SearchRequest {
+//   indexName: string
+//   params: SearchOptions & {
+//     // Additional parameters that might be sent by specific widgets
+//     facetQuery?: string
+//     facetName?: string
+//   }
+// }
+
+type SearchRequest =
+  | Parameters<SearchClient["search"]>[0]
+  | Parameters<NonNullable<SearchClient["searchForFacetValues"]>>[0]
 
 interface FacetCount {
-  value: string;
-  highlighted: string;
-  count: number;
+  value: string
+  highlighted: string
+  count: number
 }
 
 interface TypesenseFacet {
-  field_name: string;
-  counts: FacetCount[];
-}
-
-interface TypesenseFacetSearchResponse {
-  facet_counts: TypesenseFacet[];
-  search_time_ms: number;
-}
-
-interface InstantSearchFacetRequest {
-  params: {
-    facetName: string;
-    highlightPreTag?: string;
-    highlightPostTag?: string;
-  };
+  field_name: string
+  counts: FacetCount[]
 }
 
 interface FacetHit {
-  value: string;
-  highlighted: string;
-  count: number;
+  value: string
+  highlighted: string
+  count: number
 }
 
 interface AdaptedFacetSearchResponse {
-  facetHits: FacetHit[];
-  exhaustiveFacetsCount: boolean;
-  processingTimeMS: number;
+  facetHits: FacetHit[]
+  exhaustiveFacetsCount: boolean
+  processingTimeMS: number
 }
 
 export class FacetSearchResponseAdapter {
-  typesenseResponse: TypesenseFacetSearchResponse;
-  instantsearchRequest: InstantSearchFacetRequest;
-  _adaptHighlightTag: typeof utils._adaptHighlightTag;
+  typesenseResponse: SearchResult
+  instantsearchRequest: SearchRequest
+  _adaptHighlightTag: typeof utils._adaptHighlightTag
 
-  constructor(typesenseResponse: TypesenseFacetSearchResponse, instantsearchRequest: InstantSearchFacetRequest) {
-    this.typesenseResponse = typesenseResponse;
-    this.instantsearchRequest = instantsearchRequest;
-    this._adaptHighlightTag = utils._adaptHighlightTag;
+  constructor(
+    typesenseResponse: SearchResult,
+    instantsearchRequest: SearchRequest,
+  ) {
+    this.typesenseResponse = typesenseResponse
+    this.instantsearchRequest = instantsearchRequest
+    this._adaptHighlightTag = utils._adaptHighlightTag
   }
 
   _adaptFacetHits(typesenseFacetCounts: TypesenseFacet[]): FacetHit[] {
-    let adaptedResult: FacetHit[] = [];
-    const facet = typesenseFacetCounts.find((facet) => facet.field_name === this.instantsearchRequest.params.facetName);
+    let adaptedResult: FacetHit[] = []
+    const facet = typesenseFacetCounts.find(
+      (facet) =>
+        // @ts-ignore
+        facet.field_name === this.instantsearchRequest.params.facetName,
+    )
 
-    if (typeof facet !== 'undefined') {
+    if (typeof facet !== "undefined" && "params" in this.instantsearchRequest) {
       adaptedResult = facet.counts.map((facetCount) => ({
         value: facetCount.value,
         highlighted: this._adaptHighlightTag(
           facetCount.highlighted,
+          // @ts-ignore
           this.instantsearchRequest.params.highlightPreTag,
+          // @ts-ignore
           this.instantsearchRequest.params.highlightPostTag,
         ),
         count: facetCount.count,
-      }));
+      }))
     }
 
-    return adaptedResult;
+    return adaptedResult
   }
 
   adapt(): AdaptedFacetSearchResponse {
     const adaptedResult = {
+      // @ts-ignore
       facetHits: this._adaptFacetHits(this.typesenseResponse.facet_counts),
       exhaustiveFacetsCount: true,
       processingTimeMS: this.typesenseResponse.search_time_ms,
-    };
-    return adaptedResult;
+    }
+    // @ts-ignore
+    return adaptedResult
   }
 }
 
-Object.assign(FacetSearchResponseAdapter.prototype, utils);
+Object.assign(FacetSearchResponseAdapter.prototype, utils)
