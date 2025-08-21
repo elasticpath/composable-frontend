a3c6e9b1f4e0de303bf50604f5467a3d357a4b3c
Robert Field <robertfield@quadrimular.com>
2025-08-21 15:46:37 +0100

    feat: latest working example

---
+++ examples/spa-search-instantsearch/src/Hit.tsx
@@ -0,0 +1,40 @@
+import type { Hit as AlgoliaHit } from "instantsearch.js/es/types"
+
+import { Snippet } from "react-instantsearch"
+
+type HitProps = {
+  hit: AlgoliaHit<{
+    attributes: {
+      name: string
+      description?: string
+      sku: string
+      extensions: {
+        "products(Details)": {
+          "BRAND-NAME": string
+        }
+      }
+    }
+  }>
+}
+
+export function Hit({ hit }: HitProps) {
+  return (
+    <article className="hit">
+      <div className="hit-image">
+        <img src="https://placehold.co/400" alt={hit.attributes.name} />
+      </div>
+      <div>
+        <h1>
+          <Snippet hit={hit} attribute="attributes.name" />
+        </h1>
+        <div>
+          By{" "}
+          <strong>
+            {hit.attributes.extensions?.["products(Details)"]?.["BRAND-NAME"]}
+          </strong>{" "}
+          {/*in <strong>{hit.categories[0]}</strong>*/}
+        </div>
+      </div>
+    </article>
+  )
+}
