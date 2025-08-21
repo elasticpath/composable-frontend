bc4d24bfb633c4e2460b3c65894959d3ea4d0a0a
Robert Field <robertfield@quadrimular.com>
2025-08-21 10:44:04 +0100

    feat: latest spec updates

--- packages/sdks/shopper/src/client/types.gen.ts
+++ packages/sdks/shopper/src/client/types.gen.ts
@@ -7270,9 +7270,7 @@ export type MultiSearchRequest = {
 }
 
 export type SearchQuery = {
-  query_by?: string
   highlight_full_fields?: string
-  collection?: string
   q?: string
   facet_by?: string
   filter_by?: string
