0ade471878c2b20e45a9881a0a4ea88ca3e624af
Robert Field <robertfield@quadrimular.com>
2025-08-26 15:57:43 +0100

    feat: wip tests

--- packages/sdks/shopper/vitest.config.ts
+++ packages/sdks/shopper/vitest.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
+    coverage: {
+      reporter: ["text", "html"],
+      include: ["src/**/*.{ts,tsx}"],
+    },
   },
 })
