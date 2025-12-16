b8e6ca5acba9f0cb62a630c63b523514c3ecef87
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2025-12-16 17:05:24 +0500

    Update has-physical.ts
    revert to old behavior if no productDetails

--- examples/core/src/lib/has-physical.ts
+++ examples/core/src/lib/has-physical.ts
@@ -3,6 +3,7 @@ import { getACart } from "@epcc-sdk/sdks-shopper";
 export function getHasPhysicalProducts(cart: Awaited<ReturnType<typeof getACart>>["data"]) {
   return cart?.included?.items?.some(
     (item: any) =>
-      item?.productDetail?.attributes?.commodity_type === "physical"
+      // default hasPhysical to true if no product details found
+      item?.productDetail?.attributes?.commodity_type !== "digital"
   ) ?? false;
 };
